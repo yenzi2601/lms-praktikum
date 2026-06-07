db2afc344648109dcbf5afe4a59646132961fcd4
yenzi2601 <enzifihrezi@gmail.com>
2026-06-07 12:30:18 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const path = require('path');
+
+const nextConfig = {
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.resolve(__dirname);
+    return config;
+  },
+};
+
 module.exports = nextConfig;
